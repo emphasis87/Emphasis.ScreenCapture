5e14806030b6570c2e226ec5ef0db51e5f03f43c
lubos_hemala <lubos.hemala@gmail.com>
2020-02-14 22:13:42 +0100

    dxgi enum outputs benchmark

--- src/Emphasis.ScreenCapture.Tests/DxgiTests.cs
+++ src/Emphasis.ScreenCapture.Tests/DxgiTests.cs
@@ -7,14 +7,18 @@ namespace Emphasis.ScreenCapture.Tests
 {
 	public class DxgiTests
 	{
+		private const int Count = 10000;
+
+		/// <summary>
+		/// Retrieving the list of outputs is quite cheap (&lt;0.1ms).
+		/// </summary>
 		[Test]
 		public void Get_all_outputs_benchmark()
 		{
-			// Create DXGI Factory1
 			var factory = new Factory1();
 
 			var sw = Stopwatch.StartNew();
-			for (var i = 0; i < 1000; i++)
+			for (var i = 0; i < Count; i++)
 			{
 				var adapters = factory.Adapters;
 				foreach (var adapter in adapters)
@@ -22,13 +26,13 @@ namespace Emphasis.ScreenCapture.Tests
 					var outputs = adapter.Outputs;
 					foreach (var output in outputs)
 					{
-						
+
 					}
 				}
 			}
 			sw.Stop();
 
-			var microseconds = (sw.ElapsedTicks/1000) / (TimeSpan.TicksPerMillisecond / 1000);
+			var microseconds = (sw.ElapsedTicks/Count) / (TimeSpan.TicksPerMillisecond / 1000);
 			Console.WriteLine($"Collecting outputs: {microseconds}microseconds");
 		}
 	}
