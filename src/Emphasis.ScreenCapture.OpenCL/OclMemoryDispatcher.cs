e61b66a9bbef443ee8da96029538a6b1f2f98ad9
lubos_hemala <lubos.hemala@gmail.com>
2020-02-15 22:43:14 +0100

    opencl

---
+++ src/Emphasis.ScreenCapture.OpenCL/OclMemoryDispatcher.cs
@@ -0,0 +1,13 @@
+﻿using Cloo;
+using SharpDX.Direct3D11;
+
+namespace Emphasis.ScreenCapture.OpenCL
+{
+	public class OclMemoryDispatcher
+	{
+		public ComputeBuffer<T> Dispatch<T>(ScreenCapture capture, ComputePlatform target)
+		{
+
+		}
+	}
+}
