e62b2ab3f08d62f48da251b35c470c22046c1893
lubos_hemala <lubos.hemala@gmail.com>
2020-02-29 23:12:45 +0100

    text detector

--- src/Emphasis.TextDetection/TextDetector.cs
+++ src/Emphasis.TextDetection/TextDetector.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using Cloo;
 
 namespace Emphasis.TextDetection
 {
 	public class TextDetector
 	{
+		public void DetectText(ComputeContext context, ComputeImage2D image, int width, int height)
+		{
+
+		}
 	}
 }
