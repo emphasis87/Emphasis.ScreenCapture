f117ec00a3dc2eb1843b4b08e6621e9ab1201d46
lubos_hemala <lubos.hemala@gmail.com>
2020-03-15 01:06:51 +0100

    cv project

---
+++ src/Emphasis.ComputerVision/Greyscale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace Emphasis.ComputerVision
+{
+	public partial class Algorithms
+	{
+		public static async Task Greyscale(byte[] source, byte[] destination)
+		{
+			var simdLength = Vector<byte>.Count;
+			new Vector<byte>(source,)
+		}
+	}
+}
