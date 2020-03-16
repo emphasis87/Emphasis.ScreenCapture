5e92100967605ed868c1517cb515660f15e502fa
lubos_hemala <lubos.hemala@gmail.com>
2020-03-17 00:42:07 +0100

    sobel

--- src/Emphasis.ComputerVision/Algorithms.cs
+++ src/Emphasis.ComputerVision/Algorithms.cs
@@ -42,17 +42,72 @@ namespace Emphasis.ComputerVision
 			}
 		}
 
-		public static void Sobel(int width, int height, byte[] source, byte[] destination)
+		public static void Sobel(int width, int height, byte[] source, byte[] gradient, byte[] direction)
 		{
 			for (var y = 0; y < height; y++)
 			{
 				var i = Clamp(y, height);
 				for (var x = 0; x < width; x++)
 				{
-					var d = y * width + x;
 					var j = Clamp(x, width);
 
+					var dx = 0.0f;
+					var dxAbs = 0.0f;
+					var dy = 0.0f;
+					var dyAbs = 0.0f;
 
+					for (var channel = 0; channel < 4; channel++)
+					{
+						var cdx =
+							source[(i - 1) * (width * 4) + (j - 1) * 4 + channel] * 0.0625f +
+							source[(i - 1) * (width * 4) + (j + 0) * 4 + channel] * 0.1250f +
+							source[(i - 1) * (width * 4) + (j + 1) * 4 + channel] * 0.0625f +
+							source[(i + 0) * (width * 4) + (j - 1) * 4 + channel] * 0.1250f +
+							source[(i + 0) * (width * 4) + (j + 0) * 4 + channel] * 0.2500f +
+							source[(i + 0) * (width * 4) + (j + 1) * 4 + channel] * 0.1250f +
+							source[(i + 1) * (width * 4) + (j - 1) * 4 + channel] * 0.0625f +
+							source[(i + 1) * (width * 4) + (j + 0) * 4 + channel] * 0.1250f +
+							source[(i + 1) * (width * 4) + (j + 1) * 4 + channel] * 0.0625f;
+
+						var cdy =
+							source[(i - 1) * (width * 4) + (j - 1) * 4 + channel] * 0.0625f +
+							source[(i - 1) * (width * 4) + (j + 0) * 4 + channel] * 0.1250f +
+							source[(i - 1) * (width * 4) + (j + 1) * 4 + channel] * 0.0625f +
+							source[(i + 0) * (width * 4) + (j - 1) * 4 + channel] * 0.1250f +
+							source[(i + 0) * (width * 4) + (j + 0) * 4 + channel] * 0.2500f +
+							source[(i + 0) * (width * 4) + (j + 1) * 4 + channel] * 0.1250f +
+							source[(i + 1) * (width * 4) + (j - 1) * 4 + channel] * 0.0625f +
+							source[(i + 1) * (width * 4) + (j + 0) * 4 + channel] * 0.1250f +
+							source[(i + 1) * (width * 4) + (j + 1) * 4 + channel] * 0.0625f;
+
+						var cdxAbs = Math.Abs(cdx);
+						if (cdxAbs > dxAbs)
+						{
+							dx = cdx;
+							dxAbs = cdxAbs;
+						}
+
+						var cdyAbs = Math.Abs(cdy);
+						if (cdyAbs > dyAbs)
+						{
+							dy = cdy;
+							dyAbs = cdyAbs;
+						}
+					}
+
+					var g = Math.Sqrt(dx * dx + dy * dy);
+					var a = Math.Atan2(dy, dx) / Math.PI;
+
+					var d = y * width + x;
+					gradient[d] = Convert.ToByte(g);
+
+					// Convert the angle into 8 distinct directions
+					var dir = Convert.ToByte((a + 1.125) * 4 - 1);
+					// Indexes 0,8,9 denote the same direction
+					if (dir > 7)
+						dir = 0;
+
+					direction[d] = dir;
 				}
 			}
 		}
