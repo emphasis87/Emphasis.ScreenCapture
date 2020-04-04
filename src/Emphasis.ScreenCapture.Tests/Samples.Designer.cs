60f2a11dccf7eb06767d8937524f7769605621f3
lubos_hemala <lubos.hemala@gmail.com>
2020-04-04 14:16:45 +0200

    dark on white sample

--- src/Emphasis.ScreenCapture.Tests/Samples.Designer.cs
+++ src/Emphasis.ScreenCapture.Tests/Samples.Designer.cs
@@ -89,5 +89,15 @@ namespace Emphasis.ScreenCapture.Tests {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap sample03 {
+            get {
+                object obj = ResourceManager.GetObject("sample03", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
