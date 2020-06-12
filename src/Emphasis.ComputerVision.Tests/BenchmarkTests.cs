7416e2106bbe617b1f1bafa99f4abbc8902044db
Luboš Hemala <lubos.hemala@gmail.com>
2020-06-12 14:36:52 +0200

    benchmark

---
+++ src/Emphasis.ComputerVision.Tests/BenchmarkTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace Emphasis.ComputerVision.Tests
+{
+	public class BenchmarkTests
+	{
+		[SimpleJob]
+		public class AlgorithmTests
+		{
+			private int channels = 4;
+			private byte[] _source;
+			private byte[] _grayscale;
+
+			[Params(1920)]
+			public int Width;
+
+			[Params(1200)]
+			public int Height;
+
+			private int N;
+			private int M;
+
+			[GlobalSetup]
+			public void Setup()
+			{
+				N = Height * Width;
+				M = N * channels;
+
+				_source = new byte[M];
+				_grayscale = new byte[M];
+			}
+
+			[Benchmark]
+			public void Grayscale()
+			{
+				Algorithms.Grayscale(Width, Height, _source, _grayscale);
+			}
+		}
+	}
+}
---
+++ src/Emphasis.ComputerVision.Tests/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace Emphasis.ComputerVision.Tests
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			BenchmarkSwitcher
+				.FromAssembly(typeof(Program).Assembly)
+				.Run();
+		}
+	}
+}
