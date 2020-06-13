27bbc1fd6b2c9a2110e49e9bb2afbd4300908a93
lubos_hemala <lubos.hemala@gmail.com>
2020-06-13 11:03:11 +0200

    convolution

---
+++ src/Emphasis.ComputerVision/Convolution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emphasis.ComputerVision
+{
+	public static partial class Algorithms
+	{
+		public static void Convolution()
+		{
+
+		}
+	}
+}
