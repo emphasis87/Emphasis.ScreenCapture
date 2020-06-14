5e67c5007bba83847a1ef321e6eb73bd34a222d1
Luboš Hemala <lubos.hemala@gmail.com>
2020-06-14 12:41:46 +0200

    benchmarks

--- src/Emphasis.ComputerVision.Tests/Benchmarks/SobelBenchmarks.cs
+++ src/Emphasis.ComputerVision.Tests/Benchmarks/SobelBenchmarks.cs
@@ -5,7 +5,7 @@ using BenchmarkDotNet.Attributes;
 
 namespace Emphasis.ComputerVision.Tests.Benchmarks
 {
-	[SimpleJob()]
+	[SimpleJob]
 	public class SobelBenchmarks
 	{
 		[Params(1920)]
@@ -13,5 +13,33 @@ namespace Emphasis.ComputerVision.Tests.Benchmarks
 
 		[Params(1200)]
 		public int Height;
+
+		public int Channels = 4;
+
+		private byte[] _source;
+		private float[] _dx;
+		private float[] _dy;
+		private float[] _gradient;
+		private float[] _angle;
+		private byte[] _neighbours;
+
+		[GlobalSetup]
+		public void Setup()
+		{
+			var n = Width * Height;
+			var m = n * Channels;
+			_source = new byte[m];
+			_dx = new float[n];
+			_dy = new float[n];
+			_gradient = new float[n];
+			_angle = new float[n];
+			_neighbours = new byte[n * 5];
+		}
+
+		[Benchmark]
+		public void Sobel3()
+		{
+			UnoptimizedAlgorithms.Sobel3(Width, Height, _source, _dx, _dy, _gradient, _angle, _neighbours, Channels);
+		}
 	}
 }
