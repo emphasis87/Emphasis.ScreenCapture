ea09f46f5ed8d8a7a8e18a17e84946d08be4f81e
Luboš Hemala <lubos.hemala@gmail.com>
2020-06-14 09:59:09 +0200

    benchmarks

---
+++ src/Emphasis.ComputerVision.Tests/Benchmarks/SobelBenchmarks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace Emphasis.ComputerVision.Tests.Benchmarks
+{
+	[SimpleJob()]
+	public class SobelBenchmarks
+	{
+		[Params(1920)]
+		public int Width;
+
+		[Params(1200)]
+		public int Height;
+	}
+}
