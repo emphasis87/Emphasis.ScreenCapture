aba7a1a1090c2b01f6634cc7415513486c4ec1b8
Luboš Hemala <lubos.hemala@gmail.com>
2020-06-17 16:47:55 +0200

    sobel

---
+++ src/Emphasis.ComputerVision/Sobel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emphasis.ComputerVision
+{
+	public partial class Algorithms
+	{
+		public void Sobel(Dimensions2D dimensions, byte[] source, byte[] dx, byte[] dy)
+		{
+
+		}
+	}
+}
