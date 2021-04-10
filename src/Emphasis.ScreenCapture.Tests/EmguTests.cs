4d397f849ea960f198c59a23affbbba531650324
lubos_hemala <lubos.hemala@gmail.com>
2021-04-11 01:41:31 +0200

    mser, but dont have running example

--- src/Emphasis.ScreenCapture.Tests/EmguTests.cs
+++ src/Emphasis.ScreenCapture.Tests/EmguTests.cs
@@ -2,7 +2,9 @@
 using System.Diagnostics;
 using Emgu.CV;
 using Emgu.CV.CvEnum;
+using Emgu.CV.Features2D;
 using Emgu.CV.Structure;
+using Emgu.CV.Util;
 using Emphasis.ScreenCapture.Helpers;
 using FluentAssertions.Extensions;
 using NUnit.Framework;
@@ -101,6 +103,55 @@ namespace Emphasis.ScreenCapture.Tests
 
 			Run("sample13.png");
 			result.RunAs(width, height, 1, "canny.png");
+		}
+
+		[Test]
+		public void MSER()
+		{
+			var sourceBitmap = Samples.sample03;
+
+			var source = sourceBitmap.ToBytes();
+
+			var w = sourceBitmap.Width;
+			var h = sourceBitmap.Height;
+
+			var data = new byte[h, w, 4];
+			for (var y = 0; y < h; y++)
+			{
+				for (var x = 0; x < w; x++)
+				{
+					var d = y * (w * 4) + x * 4;
+					for (var c = 0; c < 4; c++)
+					{
+						data[y, x, c] = source[d + c];
+					}
+				}
+			}
+			
+			var image = new Image<Bgra, byte>(data);
+
+			MSERDetector detector = new MSERDetector(
+				minArea:5);
+			var msers = new VectorOfVectorOfPoint();
+			var bboxes = new VectorOfRect();
+			detector.DetectRegions(image, msers, bboxes);
+
+			var result = new byte[w * h];
+			foreach (var mser in msers.ToArrayOfArray())
+			{
+				foreach (var point in mser)
+				{
+					result[point.Y * w + point.X] = 255;
+				}
+			}
+			foreach (var bbox in bboxes.ToArray())
+			{
+				
+			}
+
+
+			Run("sample03.png");
+			result.RunAs(w, h, 1, "mser.png");
 		}
 	}
 }
